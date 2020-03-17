8c2fcf9eb09a3bd58ae7fb32521029c532bc2903
Aja Strommen <9466667+astrommen@users.noreply.github.com>
2020-03-16 21:09:22 -0400

    got user answer to return

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -10,21 +10,66 @@ var connection = mysql.createConnection({
     database: "bamazonDB"
 });
 
+// used for decoration
+var decor = "\n________________________________________\n";
+var decor2 = "\n---------------------------------------------";
+
 // connect to mysql server and db
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
-    buyProd();
+    console.log("\nProducts for Sale" + decor);
+    prodList();
+    // setTimeout(start, 1000);
 });
 
-function buyProd() {
+function prodList() {
     // query db for all products
-    connection.query("SELECT * FROM products", function(err, results) {
+    connection.query("SELECT * FROM products", function (err, results) {
         if (err) throw err;
-
+        console.log("is in query");
         results.forEach(element => {
-            
+
             return console.log(
-                element.item_id);
+                "#" + element.item_id + " " +
+                element.product_name + " " +
+                "$" + element.price +
+                decor2
+            );
         });
+    // })
+// }
+
+
+// function start() {
+
+    inquirer.prompt([
+        {
+            name: "item",
+            type: "input",
+            message: "What item id would you like to purchase?",
+            validate: function(value) {
+                if (isNaN(value) === false) {
+                  return true;
+                }
+                return false;
+            }
+        },
+        {
+            name: "quantity",
+            type: "input",
+            message: "How many would you like to purchase?",
+            validate: function(value) {
+                if (isNaN(value) === false) {
+                  return true;
+                }
+                return false;
+            }
+        }
+    ]).then(function(answer) {
+
+        var chosenItem;
+        console.log(answer);
+
     })
+})
 }
